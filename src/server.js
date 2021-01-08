1ecd752a679a78b9ec76d4a821fef05870fab412
Corentin Bacconnais <corentin.bacconnais@gmail.com>
2021-01-08 17:36:52 +0100

    Change message

--- src/server.js
+++ src/server.js
@@ -2,5 +2,5 @@ const app = require('./app')
 const port = 1902
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`App listening in ${port}`)
 })
