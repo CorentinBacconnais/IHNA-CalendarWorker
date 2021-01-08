fb78a7f79529c56c8a09493ff6a9ad87ecab8dd5
Corentin Bacconnais <corentin.bacconnais@gmail.com>
2021-01-08 20:36:48 +0100

    Add Get Information + unit test + integration test

--- src/app.js
+++ src/app.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const app = express()
+const calendarRoutes = require('./routes/calendar_worker.routes')
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.use(express.json())
+
+app.use('/api/calendarworker', calendarRoutes)
+
+app.use((error, req, res) => {
+    res.status(500).json({message: error.message})
 })
 
 module.exports = app
--- tests/unit/sample.test.js
+++
@@ -1,6 +0,0 @@
-describe('Sample_Name', () => {
-  describe('Sample_Categorie', () => {
-    it('This is sample', () => {
-    })
-  })
-})
